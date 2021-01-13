c219ba105f0dce4b32835232e06323e6095b8989
abhinav <abhinav.drs@gmail.com>
2021-01-13 18:35:53 +0100

    hooks YOLO on the video- invoked by Run_with_Counters.py

--- API_extensions/Start_Yolo_On_Video_.py
+++ API_extensions/Start_Yolo_On_Video_.py
@@ -1,6 +1,9 @@
+"""This module hooks YOLO on the video. It is invoked by Run_with_Counters.py"""
+
 import requests
 
 start_yolo_url= 'http://localhost:8080/start'
 response_yolo_url = requests.get(url=start_yolo_url, verify=False)
 if (response_yolo_url == 'OK'):
     print("Yolo is hooked on the video")
+
