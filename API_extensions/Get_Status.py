aa3c4b02761434f9a6ab4add5cf24772a47b2592
abhinav <abhinav.drs@gmail.com>
2021-01-13 18:40:28 +0100

    Returns recording id assigned to the current video

--- API_extensions/Get_Status.py
+++ API_extensions/Get_Status.py
@@ -1,3 +1,5 @@
+"""Returns recording id assigned to the current video. It is invoked by Run_with_Counters.py."""
+
 import requests
 import json
 get_status_url= 'http://localhost:8080/status'
@@ -5,6 +7,3 @@ response_get_status_url = requests.get(url=get_status_url, verify=False)
 
 print("Recording ID : ", json.dumps(response_get_status_url.json()["appState"]["recordingStatus"]["recordingId"] , indent=4, sort_keys=True))
 
-#recording_id = json.dumps(response_get_status_url.json())
-#print(recording_id["recordingId"])
-#print(recording_id)
