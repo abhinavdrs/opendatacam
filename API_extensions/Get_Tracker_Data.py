bc6686cfb20f9077cd5ad44570d7ff27dbda3d1b
abhinav <abhinav.drs@gmail.com>
2021-01-13 20:10:47 +0100

    Retrieves tracker data and converts it to desired .csv format

--- API_extensions/Get_Tracker_Data.py
+++ API_extensions/Get_Tracker_Data.py
@@ -1,8 +1,15 @@
+"""Retrieves the tracker data generated from currently recorded video in .json format and converts it to the desired .csv format."""
+#takes recording_ID as input.
+
 import sys
 import requests
 import json
 from pandas.io.json import json_normalize
 from pathlib import Path
+
+#tracker_dir is the directory where the tracking areas would  will be stored
+tracker_dir = 'your_directory_of_choice/'
+
 #print number of arguments and their list representation
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
@@ -26,10 +33,12 @@ print(response)
 dirty_data = response.json()
 #print(dirty_data)
 
+#transform data in the desired format
 clean_data = json_normalize(data = dirty_data, record_path= 'objects', meta = ['_id' , 'recordingId' , 'timestamp'])
+tracker_file_name = 'rec_id_tracker_data.csv'
+tracker_file_name = tracker_file_name.replace("rec_id",recording_id)
 
-arg_csv_path = '/home/abhinav/tool3/data/Tracker_Data/rec_id_tracker_data.csv'
-csv_path = arg_csv_path.replace("rec_id",recording_id)
+csv_path = os.path.join(tracker_dir,tracker_file_name)
 clean_data.to_csv(csv_path)
 
 
