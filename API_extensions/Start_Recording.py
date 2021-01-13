4c94e956a94acc7480989a2ad5d6226d026005de
abhinav <abhinav.drs@gmail.com>
2021-01-13 19:05:08 +0100

    starts recording

--- API_extensions/Start_Recording.py
+++ API_extensions/Start_Recording.py
@@ -1,3 +1,4 @@
+""" Starts recording on the current video """
 import requests
 import datetime
 start_recording_url= 'http://localhost:8080/recording/start'
@@ -8,3 +9,4 @@ if (response_start_recording_url.status_code == 200):
         text.write(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
         print("SUCCESS: Recording is Started, timestamp created in /home/abhinav/tool3/data/Counter_Areas/timestamp.txt ")
         print("Success Code", response_start_recording_url.status_code)
+
