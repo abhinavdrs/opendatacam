e4d3673141f028aa5dad34d1007ffb1b631a8f4b
abhinav <abhinav.drs@gmail.com>
2021-01-13 18:54:56 +0100

    stores counter locations to a user specified directory

--- API_extensions/Get_Counter_Areas.py
+++ API_extensions/Get_Counter_Areas.py
@@ -3,14 +3,19 @@ import requests
 import json
 
 #counter_dir is the directory where the counter_areas will be stored
-counter_dir = 'your_directory_of_choice'
+
+counter_dir = 'your_directory_of_choice/counter_file_name.json'
+
 get_counter_url= 'http://localhost:8080/counter/areas'
+
 response_get_counter_url = requests.get(url=get_counter_url, verify=False)
+
+
 if (response_get_counter_url.status_code == 200):
     print("SUCCESS: Counter areas added" + counter_dir)
     counter_data = response_get_counter_url.json()
     #print(counter_data)
     with open(counter_dir, 'w') as json_file:
         json.dump(counter_data, json_file)
-#print(json.dumps(counter_data, indent=4, sort_keys=True))
+
 
