5af56a3d5d64f4fd66e27863164f3a8ca366e498
abhinav <abhinav.drs@gmail.com>
2021-01-13 20:16:12 +0100

    merged files

--- API_extensions/JSON_To_CSV.py
+++
@@ -1,9 +0,0 @@
-import json
-import csv
-import pandas as pd
-from pandas.io.json import json_normalize
-
-dirty_data = json.loads(open('/home/abhinav/tracker','r').read())
-
-clean_data = json_normalize(data = dirty_data, record_path= 'objects', meta = ['_id' , 'recordingId' , 'timestamp'])
-clean_data.to_csv('/home/abhinav/clean_data.csv')
